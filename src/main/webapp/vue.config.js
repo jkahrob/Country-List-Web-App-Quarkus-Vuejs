1b4c0043ffb9f0a9eb72ba8f16b298ffd841ba97
jkahrob <160254653+jkahrob@users.noreply.github.com>
2024-03-01 16:31:17 +0200

    Update vue.config.js

--- src/main/webapp/vue.config.js
+++ src/main/webapp/vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Country-List-Web-App-Quarkus-Vuejs/' : '/',
+  publicPath: '/',
 };
